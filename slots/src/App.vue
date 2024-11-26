156446333eaf500ba8beac6db51803501bd9c4d2
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-11-26 14:53:04 +0300

    slot to run html in child from parent

---
+++ slots/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+ <SlotComponent />
+
+ <SlotComponent>hello</SlotComponent>
+  <SlotComponent>there</SlotComponent>
+    <SlotComponent> i am</SlotComponent>
+    <SlotComponent> 
+      <template v-slot:header>
+           <h1>i am the header</h1>
+      </template>
+      <template v-slot:default>
+<h2>i am default</h2>
+</template>
+<template v-slot:footer>
+<h1>footer here</h1>
+</template>
+    </SlotComponent>
+</template>
+
+<script>
+import SlotComponent from './components/slot'
+
+export default {
+  name: 'App',
+  components: {
+    SlotComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ slots/src/components/slot.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+<slot>heyeeyyy</slot>
+<div>
+    <slot  name="footer"></slot>
+</div>
+<div >
+    <slot name='header'></slot>
+</div>
+<div >
+    <!-- you can leave one unamed slot -->
+    <slot></slot>
+</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'SlotComponent',
+        props:['content']
+    }
+</script>
+
+<style scoped>
+
+</style>
